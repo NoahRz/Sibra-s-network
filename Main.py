8e15f89391a12cbb2cd0f3681414f3c5894e5a18
Noah Razafindrabe <noah.razafindrabe@outlook.com>
2020-02-01 20:36:37 +0100

    optimisation

--- Main.py
+++ Main.py
@@ -1,13 +1,15 @@
 from Graph import Graph
 
+
 def dates2dic(dates):
     dic = {}
     splitted_dates = dates.split("\n")
-    #print(splitted_dates)
+    # print(splitted_dates)
     for stop_dates in splitted_dates:
         tmp = stop_dates.split(" ")
         dic[tmp[0]] = tmp[1:]
     return dic
+
 
 def main():
     data_file_name1 = '1_Poisy-ParcDesGlaisins.txt'
@@ -33,33 +35,21 @@ def main():
     run = True
     size = len(graph.bus_stops)
     while run:
-        print("START :") # we enter the bus stop's number in case that the user doesn't know how to write the bus stop name
-        for bus_stop in graph.bus_stops:
-            print(graph.bus_stops.index(bus_stop), bus_stop.name)
+        print("START :")
+        # we enter the bus stop's number in case that the user doesn't know how to write the bus stop name
+        display(graph.bus_stops)
         print("Select a number :")
         ans = input()
         if 0 <= int(ans) < size:
             print("END")
             # print again the list of bus stop with the start selected
-            for bus_stop in graph.bus_stops:
-                if int(ans) == graph.bus_stops.index(bus_stop):
-                    print('\033[1;34m'+ str(graph.bus_stops.index(bus_stop)) + " " + bus_stop.name + '\033[0m') # display, blue text color
-                else:
-                    print(graph.bus_stops.index(bus_stop),bus_stop.name)
+            display(graph.bus_stops, para=ans)
             bus_stop_start = graph.bus_stops[int(ans)]
             print("Select a number:")
             ans1 = input()
             if 0 <= int(ans1) < size and ans1 != ans:
                 # print again the list of bus stop with the start and the end selected
-                for bus_stop in graph.bus_stops:
-                    if int(ans) == graph.bus_stops.index(bus_stop):
-                        print('\033[1;34m' + str(
-                            graph.bus_stops.index(bus_stop)) + " " + bus_stop.name + '\033[0m')  # display, blue text color
-                    elif int(ans1) == graph.bus_stops.index(bus_stop):
-                        print('\033[1;31m' + str(
-                            graph.bus_stops.index(bus_stop)) + " " + bus_stop.name + '\033[0m')  # display, red text color
-                    else:
-                        print(graph.bus_stops.index(bus_stop), bus_stop.name)
+                display(graph.bus_stops, para=ans, para1=ans1)
                 bus_stop_end = graph.bus_stops[int(ans1)]
                 print("Departure time ? (hour:min) :")
                 time_asked = input()
@@ -69,7 +59,7 @@ def main():
                     date_asked = "regular"
                 else:
                     date_asked = "we_holidays"
-                #path = graph.find_path_new(bus_stop_start, bus_stop_end)
+                # path = graph.find_path_new(bus_stop_start, bus_stop_end)
 
                 dist2 = graph.dijkstra2(graph.bus_stops, bus_stop_start, bus_stop_end, time_asked, date_asked)
                 print(dist2)
@@ -84,6 +74,23 @@ def main():
                 print('\x1b[0;30;41m' + 'this bus stop does not exist or it is same as the start one !' + '\x1b[0m')
         else:
             print('\x1b[0;30;41m' + 'this bus stop does not exist !' + '\x1b[0m')
+
+
+def display(bus_stops, para=None, para1=None):
+    """display the list of bus_stops
+    :param bus_stops: list og bus_stops
+    :param ans: int, answer of the user
+    :param ans1: int, answer of the user
+    """
+    for bus_stop in bus_stops:
+        if (para is not None) and int(para) == bus_stops.index(bus_stop):
+            print('\033[1;34m' + str(
+                bus_stops.index(bus_stop)) + " " + bus_stop.name + '\033[0m')  # display, blue text color
+        elif (para1 is not None) and int(para1) == bus_stops.index(bus_stop):
+            print('\033[1;31m' + str(
+                bus_stops.index(bus_stop)) + " " + bus_stop.name + '\033[0m')  # display, red text color
+        else:
+            print(bus_stops.index(bus_stop), bus_stop.name)
 
 
 if __name__ == "__main__":
